370024fba6c51c4501459b7d9e9258cb5e1b5394
yongjunhan <hanyongjun.dev@gmail.com>
2022-11-27 22:31:36 +0900

    path 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
+// const path = require('path')
+import  path  from "path";
+const __dirname = path.resolve();
+
 const webpackConfig = {
   mode : 'development',
   entry : './src/index.js',
   output : {
+    path : path.resolve(__dirname, "dist"),
     filename : "bundle.js"
   }
 }
